81c3ad4ee6e20d99e97a1afbdb375dd252c7e01f
Chloe <chloecinders@proton.me>
2025-11-17 20:25:36 +0100

    feat: add some discord styling

--- src-js/components/alert-info.ts
+++ src-js/components/alert-info.ts
@@ -1,31 +1,31 @@
 import { css, html, LitElement } from "lit";
-import { customElement, property } from 'lit/decorators.js';
+import { customElement, property } from "lit/decorators.js";
 
-@customElement('alert-info')
+@customElement("alert-info")
 export class AlertInfo extends LitElement {
     static styles = css`
         div {
-            padding: 2px 8px;
+            padding: 4px 8px;
         }
 
         .danger {
             background-color: rgba(248, 113, 113, 0.2);
-            border-left: 4px solid #F87171;
+            border-left: 4px solid #f87171;
         }
 
         .warning {
             background-color: rgba(250, 204, 21, 0.2);
-            border-left: 4px solid #FACC15;
+            border-left: 4px solid #facc15;
         }
 
         .info {
-            background-color: rgba(96, 165, 250, 0.2);
-            border-left: 4px solid #60A5FA;
+            background-color: #323c4b;
+            border-left: 4px solid #00aafc;
         }
 
         .success {
             background-color: rgba(52, 211, 153, 0.2);
-            border-left: 4px solid #34D399;
+            border-left: 4px solid #34d399;
         }
     `;
 
